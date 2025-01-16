8c0454cc84c0bc931fc1b3fa77293f10aff99921
Jurabek Abduhalilov <jurabek.tech.abduhalilov@gmail.com>
2025-01-16 16:32:31 +0500

    get elements

---
+++ app.js
@@ -0,0 +1,7 @@
+// Get Elements
+let input = document.querySelector('.converter-input');
+let unit_from = document.querySelector('#unit-from');
+let unit_to = document.querySelector('#unit-to');
+let options_from = document.querySelectorAll('#unit-from .unit-option');
+let options_to = document.querySelectorAll('#unit-to .unit-option');
+let button = document.querySelector('.converter-btn');
