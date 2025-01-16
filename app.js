d161c17621453ea4386c53b2dd11490a8856a941
Jurabek Abduhalilov <jurabek.tech.abduhalilov@gmail.com>
2025-01-16 18:10:17 +0500

    js done, project done, I am done

--- app.js
+++ app.js
@@ -11,6 +11,8 @@ let options_to = document.querySelectorAll('#unit-to .unit-option');
 
 let button = document.querySelector('.converter-btn');
 
+let result = document.querySelector('.result');
+
 // Units
 let units = ['Fahreinheit', 'Celseus', 'Kelvin'];
 let unit_from_value = '';
@@ -21,10 +23,16 @@ unit_from.addEventListener('click', () => {
     unit_from.classList.toggle('active');
 
     if (unit_from.classList.contains('active')) {
+        unit_to.classList.remove('active')
         options_from.forEach((option, index) => {
             option.addEventListener('click', () => {
                 selected_from.innerHTML = units[index]
                 unit_from_value = units[index]
+                if (input.value > 0 & unit_from_value.length > 0 & unit_to_value.length > 0) {
+                    button.disabled = false;
+                } else {
+                    button.disabled = true;
+                }
             })
         })
     }
@@ -34,11 +42,73 @@ unit_to.addEventListener('click', () => {
     unit_to.classList.toggle('active');
 
     if (unit_to.classList.contains('active')) {
+        unit_from.classList.remove('active')
         options_to.forEach((option, index) => {
             option.addEventListener('click', () => {
                 selected_to.innerHTML = units[index]
                 unit_to_value = units[index]
+                if (input.value > 0 & unit_from_value.length > 0 & unit_to_value.length > 0) {
+                    button.disabled = false;
+                } else {
+                    button.disabled = true;
+                }
             })
         })
+    }
+})
+
+// Handle Input Event
+input.addEventListener("keydown", (e) => {
+    if (e.key === "-" || e.key === "e") e.preventDefault();
+
+    if (input.value > 0 & unit_from_value.length > 0 & unit_to_value.length > 0) {
+        button.disabled = false;
+    } else {
+        button.disabled = true;
+    }
+});
+
+input.addEventListener("input", () => {
+    if (input.value <= 0) input.value = "";
+});
+
+// Handle Button Event
+button.addEventListener('click', () => {
+    if (input.value > 0 & unit_from_value.length > 0 & unit_to_value.length > 0) {
+        button.disabled = false;
+    } else {
+        button.disabled = true;
+    }
+
+    let temperature = 0;
+
+    if (unit_from_value === 'Fahreinheit' & unit_to_value === 'Celseus') {
+        temperature = (input.value - 32) * (5 / 9)
+        result.innerHTML = `${input.value} ${unit_from_value} is ${temperature} ${unit_to_value}`
+    } else if (unit_from_value === 'Fahreinheit' & unit_to_value === 'Kelvin') {
+        temperature = (input.value - 32) * (5 / 9) + 273.15
+        result.innerHTML = `${input.value} ${unit_from_value} is ${temperature} ${unit_to_value}`
+    } else if (unit_from_value === 'Celseus' & unit_to_value === 'Fahreinheit') {
+        temperature = (input.value * (9 / 5)) + 32
+        result.innerHTML = `${input.value} ${unit_from_value} is ${temperature} ${unit_to_value}`
+    } else if (unit_from_value === 'Celseus' & unit_to_value === 'Kelvin') {
+        temperature = input.value + 273.15
+        result.innerHTML = `${input.value} ${unit_from_value} is ${temperature} ${unit_to_value}`
+    } else if (unit_from_value === 'Kelvin' & unit_to_value === 'Fahreinheit') {
+        temperature = (input.value - 273.15) * (9 / 5) + 32
+        result.innerHTML = `${input.value} ${unit_from_value} is ${temperature} ${unit_to_value}`
+    } else if (unit_from_value === 'Kelvin' & unit_to_value === 'Celseus') {
+        temperature = (input.value - 273.15)
+        result.innerHTML = `${input.value} ${unit_from_value} is ${temperature} ${unit_to_value}`
+    }
+
+    console.log(temperature);
+
+    if (temperature !== 0) {
+        result.classList.remove('red')
+        return result.classList.add('green');
+    } else {
+        result.classList.add('red')
+        return result.innerHTML = 'Smth went wrong...'
     }
 })
