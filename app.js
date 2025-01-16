2adced004ff839fc3664c2b414df128dd4241254
Jurabek Abduhalilov <jurabek.tech.abduhalilov@gmail.com>
2025-01-16 16:34:22 +0500

    units added

--- app.js
+++ app.js
@@ -5,3 +5,6 @@ let unit_to = document.querySelector('#unit-to');
 let options_from = document.querySelectorAll('#unit-from .unit-option');
 let options_to = document.querySelectorAll('#unit-to .unit-option');
 let button = document.querySelector('.converter-btn');
+
+// Units
+let units = ['Fahreinheit', 'Celseus', 'Kelvin'];
