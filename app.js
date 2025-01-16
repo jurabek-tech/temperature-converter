3cc39b73615b03c7e70253f7f26752bd1ef1adf1
Jurabek Abduhalilov <jurabek.tech.abduhalilov@gmail.com>
2025-01-16 16:44:18 +0500

    units event handler

--- app.js
+++ app.js
@@ -1,10 +1,44 @@
 // Get Elements
 let input = document.querySelector('.converter-input');
+
 let unit_from = document.querySelector('#unit-from');
-let unit_to = document.querySelector('#unit-to');
+let selected_from = document.querySelector('#unit-from .unit-selected p');
 let options_from = document.querySelectorAll('#unit-from .unit-option');
+
+let unit_to = document.querySelector('#unit-to');
+let selected_to = document.querySelector('#unit-to .unit-selected p');
 let options_to = document.querySelectorAll('#unit-to .unit-option');
+
 let button = document.querySelector('.converter-btn');
 
 // Units
 let units = ['Fahreinheit', 'Celseus', 'Kelvin'];
+let unit_from_value = '';
+let unit_to_value = '';
+
+// Handle Unit Events
+unit_from.addEventListener('click', () => {
+    unit_from.classList.toggle('active');
+
+    if (unit_from.classList.contains('active')) {
+        options_from.forEach((option, index) => {
+            option.addEventListener('click', () => {
+                selected_from.innerHTML = units[index]
+                unit_from_value = units[index]
+            })
+        })
+    }
+})
+
+unit_to.addEventListener('click', () => {
+    unit_to.classList.toggle('active');
+
+    if (unit_to.classList.contains('active')) {
+        options_to.forEach((option, index) => {
+            option.addEventListener('click', () => {
+                selected_to.innerHTML = units[index]
+                unit_to_value = units[index]
+            })
+        })
+    }
+})
